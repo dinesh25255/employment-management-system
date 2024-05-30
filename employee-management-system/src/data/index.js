4711e2679efae401703852a6b88b825cee6c7f3b
dinesh25255 <dineshbhatt0707@gmail.com>
2024-05-30 10:29:32 +0545

    employment

---
+++ employee-management-system/src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'dinesh',
+    lastName: 'bhatt',
+    email: 'dinesh@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Basant',
+    lastName: 'joshi',
+    email: 'Basant@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Nisha',
+    lastName: 'Bhatt',
+    email: 'Nisha@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'kusum',
+    lastName: 'Bist',
+    email: 'kusum@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'kusum',
+    lastName: 'bist',
+    email: 'kusum@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'ganesh',
+    lastName: 'bhatt',
+    email: 'ganesha@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'jushi',
+    lastName: 'joshi',
+    email: 'jushi@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'ram',
+    lastName: 'bhatt',
+    email: 'ram@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'shyam',
+    lastName: 'bhatt',
+    email: 'shyam@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'hari',
+    lastName: 'bhatt',
+    email: 'hari@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
